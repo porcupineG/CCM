23e15317bfab08e76b9b44bb021569fb49cfd8fd
Przemyslaw Czerepaniak <p.czerepaniak@gmail.com>
2012-06-29 09:25:52 +0200

    First commit

---
+++ Debug/main.d
@@ -0,0 +1,30 @@
+main.d main.o: ../main.cpp ../easykf/ukf.h \
+ /home/porcupine/ccm/CCM/easykf/ukf_math.h \
+ /home/porcupine/ccm/CCM/easykf/ukf_types.h \
+ /home/porcupine/ccm/CCM/easykf/ukf_samples.h \
+ /home/porcupine/ccm/CCM/easykf/ukf_parameter_scalar.h \
+ /home/porcupine/ccm/CCM/easykf/ukf_parameter_ndim.h \
+ /home/porcupine/ccm/CCM/easykf/ukf_state_ndim.h \
+ /home/porcupine/ccm/CCM/easykf/ukf_sr_state_ndim.h \
+ /home/porcupine/ccm/CCM/easykf/ekf.h \
+ /home/porcupine/ccm/CCM/easykf/ekf_types.h
+
+../easykf/ukf.h:
+
+/home/porcupine/ccm/CCM/easykf/ukf_math.h:
+
+/home/porcupine/ccm/CCM/easykf/ukf_types.h:
+
+/home/porcupine/ccm/CCM/easykf/ukf_samples.h:
+
+/home/porcupine/ccm/CCM/easykf/ukf_parameter_scalar.h:
+
+/home/porcupine/ccm/CCM/easykf/ukf_parameter_ndim.h:
+
+/home/porcupine/ccm/CCM/easykf/ukf_state_ndim.h:
+
+/home/porcupine/ccm/CCM/easykf/ukf_sr_state_ndim.h:
+
+/home/porcupine/ccm/CCM/easykf/ekf.h:
+
+/home/porcupine/ccm/CCM/easykf/ekf_types.h:
---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jun 29, 2012
+ *      Author: porcupine
+ */
+
+#include <iostream>
+#include "easykf/ukf.h"
+
+using namespace std;
+using namespace ukf;
+
+int main() {
+	cout << "CCM Project - EKF frequency tracking" << endl;
+
+	return 0;
+}
+
